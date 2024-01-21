6b8b9e075da4099bff2706dc27c4e6d671922acb
princessayewah <aye23005@byui.edu>
2024-01-21 06:59:57 -0800

    week 1 added

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int user_number = -1;
+        do {
+            Console.Write("Enter a list of numbers, type 0 when finished: ");
+            user_number = int.Parse(Console.ReadLine());
+
+            if (user_number != 0)
+            {
+            numbers.Add(user_number);  
+            }
+        } while (user_number != 0);
+        
+        //COMPUTE SUM
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is {sum}");
+
+        //COMPUTE AVERAGE
+        float average = ((float)sum) / numbers.Count; 
+        Console.WriteLine($"The average is {average}");
+
+        //COMPUTE MAX
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The largest is {max}");
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int squaredNumber = SquareNumber(userNumber);
+        DisplayResult(userName, squaredNumber);
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please, enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please, enter your number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    static void DisplayResult(string name, int squared_number)
+    {
+        Console.WriteLine($"{name}, the square of your number is {squared_number}");
     }
 }
